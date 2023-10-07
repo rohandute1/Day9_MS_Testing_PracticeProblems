390f81b5af8ee86aa7b1891295e2982f501d263c
Rohan <rohandute1@gmail.com>
2023-10-07 21:51:52 +0530

    [rohan] Successfully added initial welcome message

---
+++ Day9_MS_Testing_PracticeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_MS_Testing_PracticeProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ms-Testing Problems.");
+
+            Console.ReadLine();
+        }
+    }
+}
